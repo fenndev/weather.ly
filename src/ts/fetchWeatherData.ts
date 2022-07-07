9bca10f25c893b3c209123ee3ab6122621f4d719
fenndev <fenndev@protonmail.com>
2022-07-07 05:56:04 -0700

    Created class to hold fetched weather data

--- src/ts/fetchWeatherData.ts
+++ src/ts/fetchWeatherData.ts
@@ -1,9 +1,48 @@
-export default async function fetchWeatherData(request: any) {
+export async function fetchWeatherData(request: any, units: string = 'imperial') {
     try {
         const response = await (await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${request.cityName}&limit=1&appid=${request.key}`)).json();
-        return await (await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${response[0].lat}&lon=${response[0].lon}&appid=${request.key}`)).json();
+        const data = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${response[0].lat}&lon=${response[0].lon}&units=${units}&appid=${request.key}`)).json();
+        return new WeatherData(data.name, Number.parseFloat((data.main.temp).toFixed(1)), data.weather[0].description, Number.parseFloat((data.wind.speed).toFixed(1)), data.main.humidity, units);
     }
     catch(error) {
         console.log(error);
     }
 }
+
+class WeatherData {
+        cityName: string;
+        temperature: number;
+        weatherDescription: string;
+        windSpeed: number;
+        humidity: number;
+        units: string;
+    constructor(
+        cityName: string,
+        temperature: number,
+        weatherDescription: string,
+        windSpeed: number,
+        humidity: number,
+        units: string,
+    ) {
+        this.cityName = cityName;
+        this.temperature = temperature;
+        this.weatherDescription = weatherDescription;
+        this.windSpeed = windSpeed;
+        this.humidity = humidity;
+        this.units = units;
+    }
+
+    public convertUnits() {
+        if (this.units === 'imperial') {
+            this.temperature = Number.parseFloat(((this.temperature - 32) * (5 / 9)).toFixed(1));
+            this.windSpeed = Number.parseFloat((this.windSpeed * 1.609).toFixed(1));
+            this.units = 'metric';
+        }
+        else {
+            this.temperature = Number.parseFloat((this.temperature * (9 / 5) + 32).toFixed(1));
+            this.windSpeed = Number.parseFloat((this.windSpeed / 1.609).toFixed(1));
+            this.units = 'imperial';
+        }
+    }
+
+};
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,12 +1,12 @@
 import '../style.scss';
-import fetchWeatherData from './fetchWeatherData';
+import {fetchWeatherData} from './fetchWeatherData';
 import Request from './request';
-
-// Instantiate a new Request object
 
 async function main() {
     const request = new Request('53eb90610b23be70589bc3e845c27b5a', true, 'London');
     const weatherData = await fetchWeatherData(request);
+    console.log(weatherData);
+    weatherData.convertUnits();
     console.log(weatherData);
 }
 
