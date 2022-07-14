c91f036bef4befbf0eb5319d91333ffb062c5934
fenndev <fenndev@protonmail.com>
2022-07-14 00:59:37 -0700

    Created UIManager to generate HTML

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -3,13 +3,16 @@ import WeatherData from './components/WeatherData';
 import { conditions } from './components/Conditions';
 import {fetchWeatherData} from './functions/FetchWeatherData';
 import getFullCountryName from './functions/GetFullCountryName';
+import UIManager from './classes/UIManager';
 
 async function main() {
-    let weatherData = await fetchWeatherData('berlin');
-    console.log(weatherData);
-    console.log(getFullCountryName(weatherData.countryName.toUpperCase()));
-    console.log(weatherData.weatherType);
-    console.log(conditions[weatherData.weatherType].codes[weatherData.weatherId]);
+    const uiManager = new UIManager();
+    uiManager.initializeSplash();
+    // let weatherData = await fetchWeatherData('berlin');
+    // console.log(weatherData);
+    // console.log(getFullCountryName(weatherData.countryName.toUpperCase()));
+    // console.log(weatherData.weatherType);
+    // console.log(conditions[weatherData.weatherType].codes[weatherData.weatherId]);
 }
 
 main();
