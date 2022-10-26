e519b7123e7beebfdf4824e933e0d3db632af72a
fenndev <fenndev@protonmail.com>
2022-10-26 01:12:04 -0700

    Began testing backend querying

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,23 @@
 import { render } from 'preact'
-import { App } from './app'
-import './index.css'
 
-render(<App />, document.getElementById('app') as HTMLElement)
+const serverURL = `http://weatherly-backend-fenndev.vercel.app`
+async function queryWeather(data: string) {
+    const requestURL = `${serverURL}/q=${data}`
+    const response = await fetch((requestURL), {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'text/plain',
+        },
+    })
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+    return jsonResponse;
+}
+
+queryWeather('tacoma,washington')
+
+render(
+    <>
+        <h1>Hello, world!</h1>
+    </>, document.getElementById('app') as HTMLElement)
