7f705301c687a2d6a3f81bc8390984dd704092e7
fenndev <fenndev@proton.me>
2023-01-02 19:11:43 -0800

    Further removed unused classes and reorganized

---
+++ src/scripts/index.ts
@@ -0,0 +1,12 @@
+// import '../sass/style.scss';
+// import WeatherData from './components/WeatherData';
+// // import { conditions } from './components/Conditions';
+// import {fetchWeatherData} from './functions/FetchWeatherData';
+// // import getFullCountryName from './functions/GetFullCountryName';
+// import UIManager from './classes/UIManager';
+
+// async function main() {
+
+// }
+
+// main();
