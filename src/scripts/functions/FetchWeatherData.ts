d0552dd352f5238051991aac85f98bdeb49288ca
fenndev <fenndev@proton.me>
2023-01-16 22:35:33 -0800

    Removed unneeded files and changed variable names

--- src/scripts/functions/FetchWeatherData.ts
+++ src/scripts/functions/FetchWeatherData.ts
@@ -6,13 +6,13 @@ export async function fetchWeatherData(cityName: string, units = 'metric'): Prom
     try {
         const coordinatesResponse = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${key}`);
         const coordinates: any[] = await coordinatesResponse.json();
-        const city: any = coordinates[0];
-        const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&units=${units}&appid=${key}`);
+        const location: any = coordinates[0];
+        const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&units=${units}&appid=${key}`);
         const weather = await weatherResponse.json();
         return new WeatherInstance(
-            city.name.toLowerCase(),
-            city.state.toLowerCase(),
-            city.country.toLowerCase(),
+            location.name.toLowerCase(),
+            location.state.toLowerCase(),
+            location.country.toLowerCase(),
             Number.parseFloat((weather.main.temp).toFixed(1)),
             weather.weather[0].main.toLowerCase(),
             weather.weather[0].id,
