067adf5af5885d7096319308aadc3c8d637ab525
fenndev <fenndev@proton.me>
2023-01-16 23:05:28 -0800

    Removed legacy UI Manager code and extends Subject

--- src/scripts/classes/Subject.ts
+++ src/scripts/classes/Subject.ts
@@ -6,7 +6,6 @@ export default class Subject {
     subscribe(observer: Observer) { this.observers.push(observer) }
 
     unsubscribe(observer: Observer) { this.observers = this.observers.filter(obs => obs !== observer) }
-
-
+    
     notify(data: any) { this.observers.forEach(observer => observer.update(data)) };
 }
