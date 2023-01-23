8d9467969c27fee81ce0a027de4e307a9c7db497
fenndev <fenndev@proton.me>
2023-01-23 12:01:37 -0800

    Refactor

--- src/scripts/classes/UIManager.ts
+++
@@ -1,5 +0,0 @@
-import Subject from "./Subject";
-
-export default class UIManager extends Subject {
-    
-}
---
+++ src/scripts/classes/WeatherView.ts
@@ -0,0 +1,10 @@
+import Observer from "./Observer";
+
+export default class WeatherView implements Observer {
+    constructor() {}
+
+    
+    update(data: any) {
+
+    }
+}
