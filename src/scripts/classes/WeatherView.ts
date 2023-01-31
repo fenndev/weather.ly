f57dbd0150643f26ef13b3d5e7839ec2fd0afa16
fenndev <fenndev@proton.me>
2023-01-30 18:58:24 -0800

    Began refactor of WeatherView class

--- src/scripts/classes/WeatherView.ts
+++ src/scripts/classes/WeatherView.ts
@@ -2,70 +2,27 @@ import Observer from "./utility/Observer";
 import WeatherData from "./WeatherData";
 
 export default class WeatherView implements Observer {
-    private weatherGUI?: HTMLElement;
-    private searchForm?: HTMLFormElement;
-    public searchField?: HTMLInputElement;
+    private weatherGUI!: HTMLElement;
+    private searchForm!: HTMLFormElement;
+    private searchField!: HTMLInputElement;
+
+    // Main Functions
 
     constructor() {
         this.createForm();
     }
 
-    
     update(weatherData: WeatherData | undefined) {
         if(weatherData) this.render(weatherData);
     }
-
+    
     private render(weatherData: WeatherData): void {
-        if(this.weatherGUI != null) this.clearNode(this.weatherGUI);
+        if(this.weatherGUI) this.clearNode(this.weatherGUI);
         this.createForm();
-
-        //Creating elements 
-        const weatherInfo = document.createElement("ul");
-
-        const city = document.createElement("li");
-        const cityText = document.createTextNode(`City: ${weatherData.cityName}`);
-        city.appendChild(cityText);
-        weatherInfo.appendChild(city);
-
-        if(weatherData.stateName != null) {
-            const state = document.createElement("li");
-            const stateText = document.createTextNode(`State: ${weatherData.stateName}`);
-            state.appendChild(stateText);
-            weatherInfo.appendChild(state);
-        }
-
-        const country = document.createElement("li");
-        const countryText = document.createTextNode(`Country: ${weatherData.countryName}`);
-        country.appendChild(countryText);
-        weatherInfo.appendChild(country);
-
-        const temperature = document.createElement("li");
-        const temperatureText = document.createTextNode(`Temperature: ${weatherData.temperature}`);
-        temperature.appendChild(temperatureText);
-        weatherInfo.appendChild(temperature);
-
-        const weatherType = document.createElement("li");
-        const weatherTypeText = document.createTextNode(`Weather Type: ${weatherData.weatherType}`);
-        weatherType.appendChild(weatherTypeText);
-        weatherInfo.appendChild(weatherType);
-
-        const windSpeed = document.createElement("li");
-        const windSpeedText = document.createTextNode(`Wind Speed: ${weatherData.windSpeed}`);
-        windSpeed.appendChild(windSpeedText);
-        weatherInfo.appendChild(windSpeed);
-
-        const humidity = document.createElement("li");
-        const humidityText = document.createTextNode(`Humidity: ${weatherData.humidity}`);
-        humidity.appendChild(humidityText);
-        weatherInfo.appendChild(humidity);
-
-        const units = document.createElement("li");
-        const unitsText = document.createTextNode(`Units: ${weatherData.units}`);
-        units.appendChild(unitsText);
-        weatherInfo.appendChild(units);
-
-        this.weatherGUI?.appendChild(weatherInfo);
+        this.createWeatherInfo(weatherData);
     }
+
+    // Utility Functions
 
     private clearNode(node: HTMLElement) {
         while (node.firstChild) {
@@ -73,13 +30,7 @@ export default class WeatherView implements Observer {
         }
     }
 
-    public get inputText(): string {
-        if(this.searchField?.value != undefined)
-            return this.searchField?.value;
-        return '';
-    }
-
-    private createForm() {
+    private createForm(): HTMLFormElement {
         this.weatherGUI = document.createElement("main");
         this.weatherGUI.setAttribute('id', 'gui');
         this.searchForm = document.createElement("form");
@@ -94,18 +45,61 @@ export default class WeatherView implements Observer {
         this.weatherGUI.appendChild(this.searchForm);
         this.searchForm.appendChild(this.searchField);
         this.searchForm.appendChild(submitButton);
-        this.appendForm(this.searchForm);
-    }
-
-    private appendForm(form: HTMLFormElement): void {
-        this.weatherGUI?.appendChild(form);
-    }
-
-    public get searchFormElement() {
         return this.searchForm;
     }
 
-    public get inputElement() {
-        return this.searchField;
+    private createWeatherInfo(weatherData: WeatherData): HTMLDivElement {
+        const weatherInfo = document.createElement("div");
+        const city = document.createElement("p");
+        const cityText = document.createTextNode(`City: ${weatherData.cityName}`);
+        city.appendChild(cityText);
+        weatherInfo.appendChild(city);
+
+        if(weatherData.stateName != null) {
+            const state = document.createElement("p");
+            const stateText = document.createTextNode(`State: ${weatherData.stateName}`);
+            state.appendChild(stateText);
+            weatherInfo.appendChild(state);
+        }
+
+        const country = document.createElement("p");
+        const countryText = document.createTextNode(`Country: ${weatherData.countryName}`);
+        country.appendChild(countryText);
+        weatherInfo.appendChild(country);
+
+        const temperature = document.createElement("p");
+        const temperatureText = document.createTextNode(`Temperature: ${weatherData.temperature}`);
+        temperature.appendChild(temperatureText);
+        weatherInfo.appendChild(temperature);
+
+        const weatherType = document.createElement("p");
+        const weatherTypeText = document.createTextNode(`Weather Type: ${weatherData.weatherType}`);
+        weatherType.appendChild(weatherTypeText);
+        weatherInfo.appendChild(weatherType);
+
+        const windSpeed = document.createElement("p");
+        const windSpeedText = document.createTextNode(`Wind Speed: ${weatherData.windSpeed}`);
+        windSpeed.appendChild(windSpeedText);
+        weatherInfo.appendChild(windSpeed);
+
+        const humidity = document.createElement("p");
+        const humidityText = document.createTextNode(`Humidity: ${weatherData.humidity}`);
+        humidity.appendChild(humidityText);
+        weatherInfo.appendChild(humidity);
+
+        const units = document.createElement("p");
+        const unitsText = document.createTextNode(`Units: ${weatherData.units}`);
+        units.appendChild(unitsText);
+        weatherInfo.appendChild(units);
+
+        return weatherInfo;
     }
+    
+    // Getters & Setters
+
+    public get inputText(): string { return this.searchField?.value; }
+
+    public get searchFormElement() { return this.searchForm; }
+
+    public get inputElement() { return this.searchField; }
 }
